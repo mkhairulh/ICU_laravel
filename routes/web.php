f7e6dd26143a729544fc828bfc1dbaf58130eda5
khairul hasan <khairul.hasan@icu.gov.my>
2024-10-15 10:36:43 +0800

    Update web route and home viewa

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,53 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home/{name}', function ($name) {
+    return view('home',[ 'nama'=>$name]);
+});
+
 Route::get('/home', function () {
     return view('home');
 });
 
 Route::get('/auth/signin', function () {
     return view('auth.signin');
+});
+
+Route::get('/auth/{name}/{age}', function ($name,$age) {
+    return 'User '.$name. ' age is:'.$age;
+});
+
+
+Route::get('/user/profile', function () {
+    return 'Pengguna Profiles';
+})->name('user.profile');
+
+Route::get('/user/{name}', function ($name) {
+    return 'User ' .$name;
+});
+
+//route group
+Route::prefix('food')->group(function () {
+
+    Route::get('/details', function () {
+        return 'Food details are following';
+    });
+
+    Route::get('/home', function () {
+        return 'Food home page';
+    });
+    
+});
+
+//route group
+Route::name('job')->prefix('job')->group(function () {
+
+    Route::get('home', function () {
+        return 'Job home page';
+    })->name('.home');
+
+    Route::get('/details', function () {
+        return 'Job details are following';
+    })->name('.description');
+    
 });
